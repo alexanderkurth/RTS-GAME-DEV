a557aabbdadbbfc565387653f51453ba7f9b97cf
Alexander <Alexandrekurth@outlook.com>
2019-07-09 18:39:17 +0200

    Hexagonal grid generation

---
+++ Assets/HexTile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile
+{
+    //================================Variables
+
+    private string name;
+
+    private float x;
+    private float y;
+    private float z;
+
+    //================================Constructor
+
+    public HexTile(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //================================Methods
+
+    public string CoordinatesToString()
+    {
+        return this.x.ToString() + " , " + this.z.ToString();
+    }
+
+    //================================Getters & Setters
+
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public float getX()
+    {
+        return this.x;
+    }
+
+    public float getZ()
+    {
+        return this.z;
+    }
+
+    public int getXCoord()
+    {
+        return (int) (this.x / 1.8f);
+    }
+
+    public int getZCoord()
+    {
+        return (int) (this.z / 1.565f);
+    }
+}
---
+++ Assets/HexTileMapGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTileMapGenerator : MonoBehaviour
+{
+    //================================Variables
+
+    //Hexagon Prefab
+    public GameObject hexTilePrefab;
+
+    //Map dimension
+    [SerializeField]int mapWidth = 25;
+    [SerializeField]int mapLength = 12;
+
+    //Space between tiles 
+    [SerializeField] float tileXOffset = 1.8f;
+    [SerializeField] float tileZOffset = 1.565f;
+
+    //================================Methods
+
+    void Start()
+    {
+        CreateHexTileMap();
+    }
+
+    void CreateHexTileMap()
+    {
+        for(int x = 1; x <= mapWidth; x++)
+        {
+            for(int z = 1; z <= mapLength; z++)
+            {
+                GameObject TemporaryGameObject = Instantiate(hexTilePrefab);
+                HexTile TemporaryHexTile;
+
+                if(z % 2 == 0)
+                {
+                    TemporaryGameObject.transform.position = new Vector3(x * tileXOffset, 0, z * tileZOffset );
+                    TemporaryHexTile = new HexTile(x * tileXOffset, 0, z * tileZOffset);
+                }
+                else
+                {
+                    TemporaryGameObject.transform.position = new Vector3(x * tileXOffset + tileXOffset / 2, 0, z * tileZOffset);
+                    TemporaryHexTile = new HexTile(x * tileXOffset + tileXOffset / 2, 0, z * tileZOffset);
+                }
+                SetTileInfo(TemporaryGameObject, TemporaryHexTile);
+            }
+        }
+    }
+
+    void SetTileInfo(GameObject GO, HexTile HT)
+    {
+        GO.transform.parent = transform;
+        GO.name = HT.getXCoord() + " , " + HT.getZCoord();
+    }
+    
+    void SetTileInfo(GameObject GO, int x, int z)
+    {
+        GO.transform.parent = transform;
+        GO.name = x.ToString() + " , " + z.ToString();
+    }
+}
