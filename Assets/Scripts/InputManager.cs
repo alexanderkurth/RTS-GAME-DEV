39b1f2daa8a8ce49f02484dff88b2cf53882dd93
Alexander <Alexandrekurth@outlook.com>
2019-07-25 11:35:43 +0200

    Refactor ended

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,8 +6,42 @@ public class InputManager : MonoBehaviour
 {
     //================================ Variables
 
+    public HexTile selectedHextile;
+    public HexTileManager hexTileManager;
 
     //================================ Methods
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hexTileManager = GetHexTileClicked().transform.gameObject.GetComponent<HexTileManager>();
+
+            if(!hexTileManager.IsSelected() && selectedHextile == null)
+            {
+                selectedHextile = GetHexTileClicked();
+                selectedHextile.SetSelected(true);
+                hexTileManager.SetMaterialColor(Color.red);
+            }
+            else if(selectedHextile == GetHexTileClicked())
+            {
+                selectedHextile.SetSelected(false);
+                selectedHextile = null;
+                hexTileManager.SetMaterialColor(Color.white);
+                hexTileManager = null;
+            }
+            else if(!hexTileManager.IsSelected() && selectedHextile != null)
+            {
+                selectedHextile.SetSelected(false);
+                selectedHextile.gameObject.GetComponent<HexTileManager>().SetMaterialColor(Color.white);
+
+                selectedHextile = GetHexTileClicked();
+                selectedHextile.SetSelected(true);
+                hexTileManager.SetMaterialColor(Color.red);
+            }
+
+        }
+    }
 
     public HexTile GetHexTileClicked()
     {
@@ -18,4 +52,8 @@ public class InputManager : MonoBehaviour
         else
             return null;
     }
+
+    public void SetSelectedHextile(HexTile hextile) { this.selectedHextile = hextile ; }
+
+    public HexTileManager GetHexTileManager() { return this.hexTileManager; }
 }
--- Assets/Scripts/UiInfoManager.cs
+++ Assets/Scripts/UiInfoManager.cs
@@ -4,11 +4,8 @@ public class UiInfoManager : MonoBehaviour
 {
     //================================ Variables
 
-    public HexTile selectedHextile;
-
-    public HexTileManager hextileManager;
-
     private string infoText;
+    [SerializeField] HexTileManager hextileManager;
 
     public InputManager inputManager;
 
@@ -16,48 +13,25 @@ public class UiInfoManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        hextileManager = inputManager.GetHexTileManager();
+
+        if (inputManager.GetHexTileManager() != null)
         {
-            ClickOnTile();
+            GetInformations();
         }
     }
 
-    void ClickOnTile()//[TODO] Refactor -- class Input manager + HextileManager + UiManager 
+    void GetInformations()//check hextile manager from InputManager
     {
+            string hextileName;
+            string coordinates;
+            string emptyness;
 
-        hextileManager = inputManager.GetHexTileClicked().transform.gameObject.GetComponent<HexTileManager>();
-        string hextileName;
-        string coordinates;
-        string emptyness;
-
-        hextileName = hextileManager.GetHexTile().GetHexTileName();
-        coordinates = hextileManager.GetHexTile().GetCoordinates().ToString();
-        emptyness = hextileManager.GetHexTile().IsEmpty().ToString();
-        infoText = hextileName + "\n" + coordinates + "\n" + emptyness;
-
-        if (!hextileManager.IsSelected() && selectedHextile == null)
-        {
-            selectedHextile = inputManager.GetHexTileClicked();
-            selectedHextile.SetSelected(true);
-            hextileManager.SetMaterialColor(Color.red);
-
-        }
-        else if (selectedHextile == inputManager.GetHexTileClicked())
-        {
-            selectedHextile.SetSelected(false);
-            selectedHextile = null;
-            hextileManager.SetMaterialColor(Color.white);
-        }
-        else if (!hextileManager.IsSelected() && selectedHextile != null)
-        {
-            selectedHextile.SetSelected(false);
-            selectedHextile.gameObject.GetComponent<HexTileManager>().SetMaterialColor(Color.white);
-
-            selectedHextile = inputManager.GetHexTileClicked();
-            selectedHextile.SetSelected(true);
-            hextileManager.SetMaterialColor(Color.red);
-        }
-
+            hextileName = hextileManager.GetHexTile().GetHexTileName();
+            coordinates = hextileManager.GetHexTile().GetCoordinates().ToString();
+            emptyness = hextileManager.GetHexTile().IsEmpty().ToString();
+            infoText = hextileName + "\n" + coordinates + "\n" + emptyness;
+        
     }
 
     //================================ Getters & Setters
