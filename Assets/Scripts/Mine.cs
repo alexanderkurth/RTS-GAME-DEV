64cec18fa1188d75c3701ae13ebdabdb7794c5e3
Alexander KURTH <alexandrekurth@outlook.com>
2019-08-02 23:56:34 +0200

    ressource gathering

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -15,23 +15,33 @@ public class Mine : Building
 
     //================================ Methods
 
+    private void Awake()
+    {
+        producedResource = new Resource("gold", "mineral", 1.0f);
+        production = 1;
+        isWorking = true;
+        quantite = 0;
+    }
+    void Start()
+    {
+        StartCoroutine(ExtractResource());
+        
+    }
+
     IEnumerator ExtractResource()
     {
         while(isWorking)
         {
-            yield return new WaitForSeconds(producedResource.GetExtractionTime());
-            quantite += production ;
-        }
-    }
 
-    void Start()
-    {
-        
+            yield return new WaitForSeconds(producedResource.GetExtractionTime());
+            quantite += production;
+
+        }
     }
 
     void Update()
     {
-        Debug.Log(quantite);
+
     }
 
     
