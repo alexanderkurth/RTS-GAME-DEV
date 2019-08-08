a57cf4f108a2d5b8d930dd6975f409ef592c1db2
Alexander KURTH <alexandrekurth@outlook.com>
2019-08-08 19:57:45 +0200

    Highlight improvement almost done

--- Assets/Scripts/HexTile/HexTileManager.cs
+++ Assets/Scripts/HexTile/HexTileManager.cs
@@ -9,14 +9,21 @@ public class HexTileManager : MonoBehaviour//TODO[Refactor] take all color inste
     public BuildingHandler buildingHandler;
 
     [Header("HighLight Color")]
-    [SerializeField] private int redColor;
-    [SerializeField] private int greenColor;
-    [SerializeField] private int blueColor;
+
+    [SerializeField] private float[] redColorArray;
+    [SerializeField] private float[] greenColorArray;
+    [SerializeField] private float[] blueColorArray;
+
+    [SerializeField] private float[] redColorArray2;
+    [SerializeField] private float[] greenColorArray2;
+    [SerializeField] private float[] blueColorArray2;
 
     [Header("HighLight Management")]
     [SerializeField] private bool startedFlashing = false;
     [SerializeField] private bool flashingIn = true;
     [SerializeField] private bool lookingAtObject = false;
+
+    Color[] r, g, b;
 
     public static HexTileManager ins;
 
@@ -29,22 +36,47 @@ public class HexTileManager : MonoBehaviour//TODO[Refactor] take all color inste
 
     void Start()
     {
-        redColor = (int)GetComponent<Renderer>().material.color.r;
-        greenColor = (int)GetComponent<Renderer>().material.color.g;
-        blueColor = (int)GetComponent<Renderer>().material.color.b;
+        r = new Color[GetComponent<Renderer>().materials.Length];
+        g = new Color[GetComponent<Renderer>().materials.Length];
+        b = new Color[GetComponent<Renderer>().materials.Length];
+
+
+        redColorArray = new float[GetComponent<Renderer>().materials.Length];
+        greenColorArray = new float[GetComponent<Renderer>().materials.Length];
+        blueColorArray = new float[GetComponent<Renderer>().materials.Length];
+
+        redColorArray2 = new float[GetComponent<Renderer>().materials.Length];
+        greenColorArray2 = new float[GetComponent<Renderer>().materials.Length];
+        blueColorArray2 = new float[GetComponent<Renderer>().materials.Length];
+
+        for (int i = 0; i < GetComponent<Renderer>().materials.Length; i++)
+        {
+            redColorArray[i] = GetComponent<Renderer>().materials[i].color.r;
+            greenColorArray[i] = GetComponent<Renderer>().materials[i].color.g;
+            blueColorArray[i] = GetComponent<Renderer>().materials[i].color.b;
+
+            redColorArray2[i] = GetComponent<Renderer>().materials[i].color.r;
+            greenColorArray2[i] = GetComponent<Renderer>().materials[i].color.g;
+            blueColorArray2[i] = GetComponent<Renderer>().materials[i].color.b;
+
+            r[i] = GetComponent<Renderer>().materials[i].color;
+            g[i] = GetComponent<Renderer>().materials[i].color;
+            b[i] = GetComponent<Renderer>().materials[i].color;
+
+        }
     }
 
     void Update()
     {
         if (IsSelected())
         {
-            //   GetComponent<Renderer>().material.color = new Color32((byte)redColor, (byte)greenColor, (byte)blueColor, 255);
+
             for (int i = 0; i < GetComponent<Renderer>().materials.Length; i++)
             {
-                GetComponent<Renderer>().materials[i].color = new Color32((byte)redColor, (byte)greenColor, (byte)blueColor, 255);
+                GetComponent<Renderer>().materials[i].color = new Color32((byte)redColorArray[i], (byte)greenColorArray[i], (byte)blueColorArray[i], 255);
             }
             lookingAtObject = true;
-            // Debug.Log(GetComponent<Renderer>().materials.Length);
+
             if (!startedFlashing)
             {
                 startedFlashing = true;
@@ -55,12 +87,14 @@ public class HexTileManager : MonoBehaviour//TODO[Refactor] take all color inste
         {
             startedFlashing = false;
             lookingAtObject = false;
-            //   GetComponent<Renderer>().material.color = new Color32(160, 160, 160, 255);
+
             for (int i = 0; i < GetComponent<Renderer>().materials.Length; i++)
             {
-                GetComponent<Renderer>().materials[i].color = new Color32((byte)redColor, (byte)greenColor, (byte)blueColor, 255);
+                //GetComponent<Renderer>().materials[i].color = new Color32(160, 160, 160, 255);    
+                GetComponent<Renderer>().materials[i].color = r[i];    
+                //GetComponent<Renderer>().materials[i].SetColor( new Color32((byte)redColorArray2[i], (byte)greenColorArray2[i], (byte)blueColorArray2[i], 255));
+
             }
-            lookingAtObject = true;
             StopCoroutine(FlashObject());
         }
     }
@@ -70,33 +104,35 @@ public class HexTileManager : MonoBehaviour//TODO[Refactor] take all color inste
         while (lookingAtObject)
         {
             yield return new WaitForSeconds(0.1f);
-            if (flashingIn)
+            for (int i = 0; i < GetComponent<Renderer>().materials.Length; i++)
             {
-                if (blueColor <= 160)
+                if (flashingIn)
                 {
-                    flashingIn = false;
+                    if (blueColorArray[i] <= 160)
+                    {
+                        flashingIn = false;
+                    }
+                    else
+                    {
+                        blueColorArray[i] -= 25;
+                        redColorArray[i] -= 25;
+                        greenColorArray[i] -= 25;
+                    }
                 }
-                else
+                if (!flashingIn)
                 {
-                    blueColor -= 25;
-                    redColor -= 25;
-                    greenColor -= 25;
+                    if (blueColorArray[i] >= 250)
+                    {
+                        flashingIn = true;
+                    }
+                    else
+                    {
+                        blueColorArray[i] += 25;
+                        redColorArray[i] += 25;
+                        greenColorArray[i] += 25;
+                    }
                 }
             }
-            if (!flashingIn)
-            {
-                if (blueColor >= 250)
-                {
-                    flashingIn = true;
-                }
-                else
-                {
-                    blueColor += 25;
-                    redColor += 25;
-                    greenColor += 25;
-                }
-            }
-
         }
     }
 
